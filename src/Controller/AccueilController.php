30c87e5ac7d162fa52ce75cdd71cbae40d3c4479
dodji05 <gildas31@gmail.com>
2024-06-12 15:38:47 +0100

    first commit
    first commit

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(Request $request)
+    {
+        return $this->render('front/index.html.twig', [
+            //            'adherent' => $adherent,
+            //            'form' => $form,
+        ]);
+    }
+
+    #[Route('/restaurant', name: 'app_restaurant')]
+    public function restaurant(Request $request)
+    {
+        return $this->render('front/index.html.twig', [
+            //            'adherent' => $adherent,
+            //            'form' => $form,
+        ]);
+    }
+
+    #[Route('/hebergement', name: 'app_hebergement')]
+    public function hebergement(Request $request)
+    {
+        return $this->render('front/index.html.twig', [
+            //            'adherent' => $adherent,
+            //            'form' => $form,
+        ]);
+    }
+
+    #[Route('/hebergement/details', name: 'app_hebergement_details')]
+    public function chambres(Request $request)
+    {
+        return $this->render('front/index.html.twig', [
+            //            'adherent' => $adherent,
+            //            'form' => $form,
+        ]);
+    }
+}
